26f06301bc0bccade65cb2fa1514def476878a51
zjz2022 <zjz15019702849@gmail.com>
2024-05-18 18:30:19 +0800

    update

--- leetcode/链表/2.两数相加.js
+++ leetcode/链表/2.两数相加.js
@@ -59,20 +59,27 @@ var addTwoNumbers = function (l1, l2) {
 }
 
 // @lc code=end
+
+// 定义链表
 function ListNode(val) {
   this.val = val
   this.next = null
 }
 
+// 构造链表2->4->3
 let l1 = new ListNode(2)
 l1.next = new ListNode(4)
 l1.next.next = new ListNode(3)
 
+// 构造链表5->6->4
 let l2 = new ListNode(5)
 l2.next = new ListNode(6)
 l2.next.next = new ListNode(4)
 
+// 按照题目要求处理两个链表
 let node = addTwoNumbers(l1, l2)
+
+// 根据定义的链表，输出链表
 while (node !== null) {
   console.log(node.val)
   node = node.next
--- test.js
+++
@@ -1,4 +0,0 @@
-let a = 1
-console.log('🚀 ~ a:', a)
-let b = 2
-console.log('🚀 ~ b:', b)
